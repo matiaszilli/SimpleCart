f0ad41c651418d25001cc1d0d2400285e5f47ee3
mzilli_note <matiaszilli_note@gmail.com>
2019-02-21 17:29:48 -0300

    Added function that add and delete product to Order and checkout Order

--- app/src/routes/order.js
+++ app/src/routes/order.js
@@ -71,7 +71,7 @@ app.get('/:id', (req, res) => {
             });
         
         // if id does not exists
-        if (!product)
+        if (!order)
             return res.status(500).json({
                 ok: false,
                 msj: 'Error, wrong id',
@@ -114,9 +114,178 @@ app.delete('/:id', (req, res) => {
     });
 });
 
-
-
+// Add Product to given Order
+app.post('/:id/products', (req, res) => {
+    var orderId = req.params.id;
+    // parse body request
+    var body = req.body;
+    var newProduct = {
+        _id: body.id,
+        quantity: parseInt(body.quantity, 10)
+    };
+    Order
+        .findById(orderId)
+        .exec( (err, order) => {
             
+        // if there was a error getting the Order
+        if (err)
+            return res.status(500).json({
+                ok: false,
+                msj: 'Error getting the order',
+                errors: err
+            });
 
+        // decrement Product stock
+        Product
+            .findOneAndUpdate({ _id: newProduct._id }, { $inc: { stock: -newProduct.quantity } }, {new: true }, (err) => {
+            // if there was a error getting the Order
+            if (err)
+                return res.status(400).json({
+                    ok: false,
+                    msj: 'Error updating Product stock',
+                    errors: err
+                });
+        });
+
+        // if the Order already has that product increment the quantity
+        if(order.items.id(newProduct._id)){
+            order.items.id(newProduct._id).quantity += newProduct.quantity
+        } else { // if not, add it to items
+            order.items.push(newProduct);
+        }
+
+        // save order
+        order.save(
+            (err, orderDB) => {
+    
+            // if there was a error
+            if (err)
+                return res.status(400).json({
+                    ok: false,
+                    msj: 'Error saving Order',
+                    errors: err
+                });
+            
+            // return updated order
+            return res.json({
+                ok: true,
+                data: orderDB
+            });
+        });
+    });
+});
+
+// Delete product to given Order
+app.put('/:id/products', (req, res) => {
+    var orderId = req.params.id;
+    // parse body request
+    var body = req.body;
+    var deleteProduct = body.id;
+    Order
+        .findById(orderId)
+        .exec( (err, order) => {
+            
+        // if there was a error getting the Order
+        if (err)
+            return res.status(500).json({
+                ok: false,
+                msj: 'Error getting the order',
+                errors: err
+            });
+
+
+        // if the Product exists in the Order
+        if(order.items.id(deleteProduct)){
+            var oldQuantity = order.items.id(deleteProduct).quantity;
+            order.items.id(deleteProduct).remove();
+        } else { // if not, add it to items
+            return res.status(500).json({
+                ok: false,
+                msj: 'Product does not exist in the order',
+            });
+        }
+
+        // increment Product stock
+        Product
+        .findOneAndUpdate({ _id: deleteProduct }, { $inc: { stock: oldQuantity } }, {new: true }, (err) => {
+        // if there was a error getting the Order
+        if (err)
+            return res.status(400).json({
+                ok: false,
+                msj: 'Error updating Product stock',
+                errors: err
+            });
+        });
+
+        // save order
+        order.save(
+            (err, orderDB) => {
+    
+            // if there was a error
+            if (err)
+                return res.status(400).json({
+                    ok: false,
+                    msj: 'Error saving Order',
+                    errors: err
+                });
+            
+            // return updated order
+            return res.json({
+                ok: true,
+                data: orderDB
+            });
+        });
+    });
+});
+
+// Checkout a Order
+app.get('/:id/checkout', async (req, res) => {
+    var orderId = req.params.id;
+    
+    try {
+        order = await Order.findById(orderId);
+        // items in the order
+        let orderItems = order.items;
+            
+        order.total = 0;
+        let itemId;
+        let itemQuantity;
+        let itemPrice;
+
+        // iterate over each item in the Order
+        for (const it of orderItems) {
+            // get item (product) quantity
+            itemId = it._id;
+            itemQuantity = it.quantity;
+
+            // get product price
+            let prod = await Product.findById(itemId);
+            itemPrice = prod.price;
+
+            // accumulate to order total
+            order.total += itemPrice * itemQuantity;
+        };
+        // update order status and total
+        console.log('total ',order.total);
+        order.status = 'Closed';
+
+        // save order
+        let orderDB = await order.save();
+
+        // return updated order
+        return res.json({
+            ok: true,
+            data: orderDB
+        });
+
+    } catch (err){
+        return res.status(500).json({
+            ok: false,
+            msj: 'Error checking out',
+            errors: err
+        });
+    }
+
+});
 
 module.exports = app;
